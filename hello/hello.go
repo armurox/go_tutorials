0176a02d1e7258c2878508a1ac393e7297bb9303
Arman Jasuja <arman_jasuja@yahoo.com>
2026-07-18 16:25:49 +0530

    Multiple random greetings

--- hello/hello.go
+++ hello/hello.go
@@ -11,9 +11,14 @@ func main() {
 	// Set the prefix for every log message
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Arman")
+	names := []string{"Arman", "Raghav", "Keshav"}
+	messages, err := greetings.Hellos(names)
 	if (err != nil) {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	// messages example = map[Arman:Hi Arman. Welcome! Keshav:Hi Keshav. Welcome! Raghav:Hi Raghav. Welcome!]
+	for _, message := range messages {
+		fmt.Println(message) // This prints out the value, not the key (unlike python)
+	}
+	
 }
